06f6312c3da1d7154eb48c031d57afce30307a9a
John <yangtianzhuo970425@gmail.com>
2023-10-24 12:36:00 +1300

    Adding Seed data

---
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+using Core.Entities;
+
+namespace Infrastructure.Data;
+
+public class StoreContextSeed
+{
+    public static async Task SeedAsync(StoreContext context)
+    {
+        if (!context.ProductBrands.Any())
+        {
+            var brandData = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/brands.json");
+            var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandData);
+            context.ProductBrands.AddRange(brands);
+        }
+
+        if (!context.ProductTypes.Any())
+        {
+            var typeData=await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/types.json");
+            var types = JsonSerializer.Deserialize<List<ProductType>>(typeData);
+            context.ProductTypes.AddRange(types);
+        }
+        
+        if (!context.Products.Any())
+        {
+            var productsData = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/products.json");
+            var products = JsonSerializer.Deserialize<List<Product>>(productsData);
+            context.Products.AddRange(products);
+        }
+
+        if (context.ChangeTracker.HasChanges()) await context.SaveChangesAsync();
+    }
+}
