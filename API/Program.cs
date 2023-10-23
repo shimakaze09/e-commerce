c2aae292297623b9b4d1ce9ba48e90befc6aea58
John <yangtianzhuo970425@gmail.com>
2023-10-24 12:21:36 +1300

    Applying the migrations and creating the Database at app startup

--- API/Program.cs
+++ API/Program.cs
@@ -4,17 +4,17 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Access the connection string value
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddDbContext<StoreContext>(options => options.UseSqlite(connectionString));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<StoreContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
@@ -30,5 +30,18 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+var context = services.GetRequiredService<StoreContext>();
+var logger = services.GetRequiredService<ILogger<Program>>();
+try
+{
+    await context.Database.MigrateAsync();
+}
+catch (Exception e)
+{
+    logger.LogError(e,"An error occured during migration");
+}
 
 app.Run();
