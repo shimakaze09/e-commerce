6c289c83213777dfb820d779f0b80e06377bd167
John <yangtianzhuo970425@gmail.com>
2023-10-24 01:51:19 +1300

    Adding our first API Controller

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController: ControllerBase
+{
+    [HttpGet]
+    public string GetProducts()
+    {
+        return "this weill be a list of products.";
+    }
+
+    [HttpGet("{id:int}")]
+    public string GetProduct(int id)
+    {
+        return "single product.";
+    }
+}
