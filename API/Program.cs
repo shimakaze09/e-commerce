cff63758533e93605c9e20317a62c76bd7ff9318
John <yangtianzhuo970425@gmail.com>
2023-10-24 02:16:37 +1300

    Adding a connection string

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,15 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Access the connection string value
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(options => options.UseSqlite(connectionString));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
