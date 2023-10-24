ed9ed80b53850a5e7e2aae6a6863ee59d17cddbc
John <yangtianzhuo970425@gmail.com>
2023-10-24 23:42:57 +1300

    Adding the search functionality

--- Core/Specifications/ProductSpecParams.cs
+++ Core/Specifications/ProductSpecParams.cs
@@ -3,7 +3,7 @@
 public class ProductSpecParams
 {
     private const int MaxPageSize = 50;
-    public int PageIndex { get; set; }
+    public int PageIndex { get; set; } = 1;
 
     private int _pageSize = 6;
 
@@ -16,4 +16,11 @@ public class ProductSpecParams
     public int? BrandId { get; set; }
     public int? TypeId { get; set; }
     public string? Sort { get; set; }
+    private string? _search;
+
+    public string? Search
+    {
+        get => _search;
+        set => _search = value.ToLower();
+    }
 }
--- Core/Specifications/ProductsWithFiltersForCountSpecification.cs
+++ Core/Specifications/ProductsWithFiltersForCountSpecification.cs
@@ -2,16 +2,17 @@
 
 namespace Core.Specifications;
 
-public class ProductsWithFiltersForCountSpecification : 
-    BaseSpecification<Product>
+public class
+    ProductsWithFiltersForCountSpecification : BaseSpecification<Product>
 {
-    public ProductsWithFiltersForCountSpecification(ProductSpecParams
-        productParams) : base(x =>
+    public ProductsWithFiltersForCountSpecification(
+        ProductSpecParams productParams) : base(x =>
+        (string.IsNullOrEmpty(productParams.Search) ||
+         x.Name.ToLower().Contains(productParams.Search)) &&
         (!productParams.BrandId.HasValue ||
          x.ProductBrandId == productParams.BrandId) &&
-        (!productParams.TypeId.HasValue || x.ProductTypeId ==
-            productParams.TypeId)
-    )
+        (!productParams.TypeId.HasValue ||
+         x.ProductTypeId == productParams.TypeId))
     {
     }
 }
