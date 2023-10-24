37d52e7985428fb3bdf72582733aec75f5fbac06
John <yangtianzhuo970425@gmail.com>
2023-10-24 15:41:58 +1300

    Adding a Custom Value Resolver for AutoMapper

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -12,6 +12,8 @@ public class MappingProfiles : Profile
             .ForMember(d => d.ProductBrand,
                 o => o.MapFrom(s => s.ProductBrand.Name))
             .ForMember(d => d.ProductType,
-                o => o.MapFrom(s => s.ProductType.Name));
+                o => o.MapFrom(s => s.ProductType.Name))
+            .ForMember(d => d.PictureUrl, 
+                o => o.MapFrom<ProductUrlResolver>());
     }
 }
