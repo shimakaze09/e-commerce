0225a649880af5d091aaffdec98f19b4c21780aa
John <yangtianzhuo970425@gmail.com>
2023-10-24 23:03:51 +1300

    Adding filtering functionality

--- Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -6,7 +6,12 @@ namespace Core.Specifications;
 public class
     ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product>
 {
-    public ProductsWithTypesAndBrandsSpecification(string sort)
+    public ProductsWithTypesAndBrandsSpecification(string? sort, int? brandId,
+        int? typeId)
+        : base(x =>
+            (!brandId.HasValue || x.ProductBrandId == brandId) &&
+            (!typeId.HasValue || x.ProductTypeId == typeId)
+        )
     {
         AddInclude(x => x.ProductType);
         AddInclude(x => x.ProductBrand);
@@ -14,7 +19,7 @@ public class
 
         if (!string.IsNullOrEmpty(sort))
         {
-            switch (sort)   
+            switch (sort)
             {
                 case "priceAsc":
                     AddOrderBy(p => p.Price);
