52dfc72632d19a9d83eb566436eaf9722d8a72f9
John <yangtianzhuo970425@gmail.com>
2023-10-24 13:37:09 +1300

    Creating a Generic repository and interface

---
+++ Infrastructure/Data/GenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+
+namespace Infrastructure.Data;
+
+public class GenericRepository<T>: IGenericRepository<T> where T: BaseEntity
+{
+    public Task<T> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyList<T>> ListAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
