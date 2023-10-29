c6b6ccd2947e7a273e4bf1e3e7015465233a386d
John <yangtianzhuo970425@gmail.com>
2023-10-29 18:25:47 +1300

    Adding the increment and decrement functionality

--- client/src/app/basket/basket.component.ts
+++ client/src/app/basket/basket.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {BasketService} from "./basket.service";
+import {BasketItem} from "../shared/models/basket";
 
 @Component({
   selector: 'app-basket',
@@ -8,5 +9,13 @@ import {BasketService} from "./basket.service";
 })
 export class BasketComponent {
   constructor(public basketService: BasketService) {
+  }
+
+  incrementQuantity(item: BasketItem) {
+    this.basketService.addItemToBasket(item);
+  }
+
+  removeItem(id: number, quantity: number) {
+    this.basketService.removeItemFromBasket(id, quantity);
   }
 }
--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -48,6 +48,30 @@ export class BasketService {
     this.setBasket(basket);
   }
 
+  removeItemFromBasket(id: number, quantity = 1) {
+    const basket = this.getCurrentBasketValue();
+    if (!basket) return;
+    const item = basket.items.find(x => x.id === id);
+    if (item) {
+      item.quantity -= quantity;
+      if (item.quantity === 0) {
+        basket.items = basket.items.filter(x => x.id !== id);
+      }
+      if (basket.items.length > 0) this.setBasket(basket);
+      else this.deleteBasket(basket);
+    }
+  }
+
+  deleteBasket(basket: Basket) {
+    return this.http.delete(this.baseUrl + 'basket?id=' + basket.id).subscribe({
+      next: () => {
+        this.basketSource.next(null);
+        this.basketTotalSource.next(null);
+        localStorage.removeItem('basket_id');
+      }
+    })
+  }
+
   private addOrUpdateItem(items: BasketItem[], itemToAdd: BasketItem, quantity: number): BasketItem[] {
     const item = items.find(x => x.id === itemToAdd.id);
     if (item) item.quantity += quantity;
