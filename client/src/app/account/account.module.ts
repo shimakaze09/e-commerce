21dc6d1d941bd62b9a4ff5543bb1bd4dc72d7128
John <yangtianzhuo970425@gmail.com>
2023-10-31 14:37:08 +1300

    Using reactive forms in Angular

--- client/src/app/account/account.module.ts
+++ client/src/app/account/account.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
 import {AccountRoutingModule} from "./account-routing.module";
-
+import {SharedModule} from "../shared/shared.module";
 
 
 @NgModule({
@@ -13,7 +13,9 @@ import {AccountRoutingModule} from "./account-routing.module";
   ],
   imports: [
     CommonModule,
-    AccountRoutingModule
+    AccountRoutingModule,
+    SharedModule
   ]
 })
-export class AccountModule { }
+export class AccountModule {
+}
--- client/src/app/account/login/login.component.ts
+++ client/src/app/account/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required)
+  })
+
+  constructor() {
+  }
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+  }
 
 }
