81c918f8087916068da15950b4d80f0e89a1b226
John <yangtianzhuo970425@gmail.com>
2023-11-01 19:38:38 +1300

    Updating the generic repository

--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -14,24 +14,9 @@ public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
         _context = context;
     }
 
-    public async Task<T> GetByIdAsync(int id)
+    public void Add(T entity)
     {
-        return await _context.Set<T>().FindAsync(id);
-    }
-
-    public async Task<IReadOnlyList<T>> ListAllAsync()
-    {
-        return await _context.Set<T>().ToListAsync();
-    }
-
-    public async Task<T> GetEntityWithSpec(ISpecification<T> spec)
-    {
-        return await ApplySpecification(spec).FirstOrDefaultAsync();
-    }
-
-    public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec)
-    {
-        return await ApplySpecification(spec).ToListAsync();
+        _context.Set<T>().Add(entity);
     }
 
     public async Task<int> CountAsync(ISpecification<T> spec)
@@ -39,9 +24,40 @@ public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
         return await ApplySpecification(spec).CountAsync();
     }
 
+    public void Delete(T entity)
+    {
+        _context.Set<T>().Remove(entity);
+    }
+
+    public async Task<T> GetByIdAsync(int id)
+    {
+        return await _context.Set<T>().FindAsync(id);
+    }
+
+    public async Task<T> GetEntityWithSpec(ISpecification<T> spec)
+    {
+        return await ApplySpecification(spec).FirstOrDefaultAsync();
+    }
+
+    public async Task<IReadOnlyList<T>> ListAllAsync()
+    {
+        return await _context.Set<T>().ToListAsync();
+    }
+
+    public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec)
+    {
+        return await ApplySpecification(spec).ToListAsync();
+    }
+
+    public void Update(T entity)
+    {
+        _context.Set<T>().Attach(entity);
+        _context.Entry(entity).State = EntityState.Modified;
+    }
+
     private IQueryable<T> ApplySpecification(ISpecification<T> spec)
     {
-        return SpecificationEvaluator<T>.GetQuery(_context.Set<T>()
-            .AsQueryable(), spec);
+        return SpecificationEvaluator<T>.GetQuery(
+            _context.Set<T>().AsQueryable(), spec);
     }
 }
