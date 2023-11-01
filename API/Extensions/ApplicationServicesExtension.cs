9a17906e8780ec10bb3994024a26899da3968a83
John <yangtianzhuo970425@gmail.com>
2023-11-01 19:38:21 +1300

    Implementing the Unit of work

--- API/Extensions/ApplicationServicesExtension.cs
+++ API/Extensions/ApplicationServicesExtension.cs
@@ -27,6 +27,7 @@ public static class ApplicationServicesExtension
         services.AddScoped<IProductRepository, ProductRepository>();
         services.AddScoped<ITokenService, TokenService>();
         services.AddScoped<IOrderService, OrderService>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
         services.AddScoped(typeof(IGenericRepository<>),
             typeof(GenericRepository<>));
         services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
