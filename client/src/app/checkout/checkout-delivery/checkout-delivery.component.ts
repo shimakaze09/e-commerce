ecc9869bedbbed39ecf0315562582189ad140302
John <yangtianzhuo970425@gmail.com>
2023-11-02 16:50:19 +1300

    Updating the shipping price

--- client/src/app/checkout/checkout-delivery/checkout-delivery.component.ts
+++ client/src/app/checkout/checkout-delivery/checkout-delivery.component.ts
@@ -1,7 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {FormGroup} from "@angular/forms";
-import {CheckoutService} from "../checkout.service";
-import {DeliveryMethod} from "src/app/shared/models/deliveryMethod";
+import {FormGroup} from '@angular/forms';
+import {BasketService} from 'src/app/basket/basket.service';
+import {DeliveryMethod} from 'src/app/shared/models/deliveryMethod';
+import {CheckoutService} from '../checkout.service';
 
 @Component({
   selector: 'app-checkout-delivery',
@@ -12,7 +13,7 @@ export class CheckoutDeliveryComponent implements OnInit {
   @Input() checkoutForm?: FormGroup;
   deliveryMethods: DeliveryMethod[] = [];
 
-  constructor(private checkoutService: CheckoutService) {
+  constructor(private checkoutService: CheckoutService, private basketService: BasketService) {
   }
 
   ngOnInit(): void {
@@ -20,4 +21,9 @@ export class CheckoutDeliveryComponent implements OnInit {
       next: dm => this.deliveryMethods = dm
     })
   }
+
+  setShippingPrice(deliveryMethod: DeliveryMethod) {
+    this.basketService.setShippingPrice(deliveryMethod);
+  }
+
 }
