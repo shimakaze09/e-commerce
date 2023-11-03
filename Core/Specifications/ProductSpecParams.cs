10fc18fda7305d99b8fe2d981e2dc99480e1e419
John <yangtianzhuo970425@gmail.com>
2023-11-03 13:36:36 +1300

    Creating order module

--- Core/Specifications/ProductSpecParams.cs
+++ Core/Specifications/ProductSpecParams.cs
@@ -16,9 +16,9 @@ public class ProductSpecParams
     public int? BrandId { get; set; }
     public int? TypeId { get; set; }
     public string Sort { get; set; }
-    private string _search;
+    private string? _search;
 
-    public string Search
+    public string? Search
     {
         get => _search;
         set => _search = value.ToLower();
--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {authGuard} from './core/guards/auth.guard';
 import {NotFoundComponent} from './core/not-found/not-found.component';
 import {ServerErrorComponent} from './core/server-error/server-error.component';
 import {TestErrorComponent} from './core/test-error/test-error.component';
 import {HomeComponent} from './home/home.component';
-import {authGuard} from "./core/guards/auth.guard";
 
 const routes: Routes = [
   {path: '', component: HomeComponent, data: {breadcrumb: 'Home'}},
@@ -17,6 +17,11 @@ const routes: Routes = [
     path: 'checkout',
     canActivate: [authGuard],
     loadChildren: () => import('./checkout/checkout.module').then(m => m.CheckoutModule)
+  },
+  {
+    path: 'orders',
+    canActivate: [authGuard],
+    loadChildren: () => import('./orders/orders.module').then(m => m.OrdersModule)
   },
   {path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule)},
   {path: '**', redirectTo: '', pathMatch: 'full'},
