9c534bce3f869f54ac9b66373a0dc70fc8929019
John <yangtianzhuo970425@gmail.com>
2023-11-04 16:29:14 +1300

    Implementing the payment intent

--- Core/Entities/CustomerBasket.cs
+++ Core/Entities/CustomerBasket.cs
@@ -13,4 +13,7 @@ public class CustomerBasket
 
     public string Id { get; set; }
     public List<BasketItem> Items { get; set; } = new();
+    public int? DeliveryMethodId { get; set; }
+    public string ClientSecret { get; set; }
+    public string PaymentIntentId { get; set; }
 }
